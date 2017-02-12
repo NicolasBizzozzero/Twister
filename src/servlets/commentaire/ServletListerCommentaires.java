16702c9620d84d9d5a99896ca518072b7fa73b96
AlexiaBourmaud <alexia.bourmaud@live.fr>
2017-02-12 19:07:39 +0100

    Add files via upload

---
+++ src/servlets/commentaire/ServletListerCommentaires.java
@@ -0,0 +1,32 @@
+package servlets.commentaire;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import services.commentaire.ListerCommentaires;
+
+
+
+public class ServletListerCommentaires extends HttpServlet {
+	    public void doGet(HttpServletRequest requete, HttpServletResponse reponse) throws ServletException, IOException {
+	        // Recuperation des parametres
+	        String cle = requete.getParameter("cle");
+	        int limite = Integer.parseInt(requete.getParameter("limite"));
+	        int index_debut = Integer.parseInt(requete.getParameter("index_debut"));
+
+	        // Utilisation du service approprie
+	        JSONObject retour = ListerCommentaires.listerCommentaires(cle, index_debut, limite);
+	        reponse.setContentType("application/json");
+	        reponse.getWriter().print(retour);
+	    
+	    }
+
+}
+
+
---
+++ src/servlets/commentaire/ServletSupprimerCommentaire.java
@@ -0,0 +1,29 @@
+package servlets.commentaire;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import services.commentaire.SupprimerCommentaire;
+
+
+
+public class ServletSupprimerCommentaire extends HttpServlet {
+     public void doGet(HttpServletRequest requete, HttpServletResponse reponse) throws ServletException, IOException {
+	    // Recuperation des parametres
+        String cle = requete.getParameter("cle");
+        String contenu = requete.getParameter("contenu"); 
+
+        // Utilisation du service approprie
+        JSONObject retour = SupprimerCommentaire.supprimerCommentaire(cle, contenu);
+        reponse.setContentType("application/json");
+        reponse.getWriter().print(retour);
+    
+     }
+
+}
