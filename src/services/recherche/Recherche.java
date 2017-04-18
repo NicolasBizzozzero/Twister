4d04ffdc36f129d5f481c751239028b0f8b311c6
NicolasBi <nicolasbizzozzero@gmail.com>
2017-04-18 23:17:47 +0200

    debut fonction recherche

--- src/services/recherche/Recherche.java
+++ src/services/recherche/Recherche.java
@@ -16,68 +16,91 @@ import com.mongodb.MapReduceCommand;
 import com.mongodb.MapReduceOutput;
 import com.mongodb.util.JSON;
 
+import outils.Noms;
+
 public class Recherche {
-//	private final static String map = "function() {" +
-//						              "    var text = \"%s\";" +
-//							          "    var words = text.match(/\\w+/g);" +
-//							          "    var tf = {};" +
-//							          "    for (var i = 0; i < words.length; i++) {" +
-//							          "        if (tf[words[i]] == null) {" +
-//							          "            tf[words[i]] = 1;" +
-//							          "        } else {" +
-//							          "            tf[words[i]] += 1;" +
-//							          "        }" +
-//							          "    }" +
-//							          "    for (w in tf) {" +
-//							          "        emit(w, 1);" +
-//							          "    }" +
-//							          "}";
-//	
-//	private final static String reduce = "function(k, v) {" +
-//							             "    v = v.split(\",\")" +
-//							             "    var df = v.lenth;" +
-//							             "    var ret = [];" +
-//							             "    for (value in v) {" +
-//							             "        for (k in v[value]) {" +
-//							             "            ret[k] = v[value][k];" +
-//							             "        }" +
-//							             "    }" +
-//							             "    return ret.toString();" +
-//							             "}";
-//	
-//	private static final String finalize = "function(k, v) {" +
-//								           "    var df = v.length;" +
-//								           "    for (d in v) {" +
-//								           "        v[d] = v[d] * Math.log(N/parseFloat(df));" +
-//								           "    }" +
-//								           "    return v;" +
-//								           "}";
-//
-//    public static JSONArray MapReduce(String query) throws UnknownHostException {
-//
-//        String map_function = String.format(map, query);
-//        String reduce_function = reduce;
-//        String finalize_function = finalize;
-//
-//        DBCollection collection = bd.tools.MessagesTools.getCollectionMessages();
-//
-//        MapReduceCommand cmd = new MapReduceCommand(collection, map, reduce, "out", MapReduceCommand.OutputType.REPLACE, null);
-//        cmd.setFinalize(finalize);
-//        BasicDBObject obj = new BasicDBObject();
-//        obj.put("N", collection.count());
-//        cmd.setScope(obj);
-//
-//        MapReduceOutput out = collection.mapReduce(cmd);
-//        JSONArray result = new JSONArray();
-//        for (DBObject o : out.results()) {
-//            try {
-//                JSONObject message = new JSONObject(JSON.serialize(o));
-//                result.put(message);
-//            } catch (JSONException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }
-//        }
-//        return result;
-//    }
+	// N est une variable globale contenant le nombre de documents au total
+	
+	private final static String map = "function() {" +
+	                                  "    var id = this.id_message;" +
+						              "    var text = this.contenu;" +			// /!\
+							          "    var words = text.match(/\\w+/g);" +
+							          "    var tf = {};" +
+							          "    for (var i = 0; i < words.length; i++) {" +
+							          "        if (tf[words[i]] == null) {" +
+							          "            tf[words[i]] = 1;" +
+							          "        } else {" +
+							          "            tf[words[i]] += 1;" +
+							          "        }" +
+							          "    }" +
+							          "    for (w in tf) {" +
+							          "        var ret = {};" +
+							          "        ret[id] = tf[w];" +
+							          "        emit(w, ret);" +
+							          "    }" +
+							          "}";
+	
+	private final static String reduce = "function(key, values) {" +
+							             "    var df = values.length;" +
+							             "    var res = {};" +
+							             "    for (var index in values) {" +
+							             "        for (var k in values[index]) {" +
+							             "            res[k] = values[index][k];" +
+							             "        }" +
+							             "    }" +
+							             "    return res;" +
+							             "}";
+	
+	private static final String finalize = "function(k, v) {" +
+								           "    var df = Object.keys(v).length;" +
+								           "    for (var d in v) {" +
+								           "        v[d] = v[d] * Math.log(N / df);" +
+								           "    }" +
+								           "    return v;" +
+								           "}";
+
+    public static JSONArray MapReduce(String query) throws UnknownHostException {
+    	// On recupere les fonctions
+        String mapFunction = String.format(map, query);
+        String reduceFunction = reduce;
+        String finalizeFunction = finalize;
+        
+        // On recupere les collections
+        DBCollection collectionMessages = bd.tools.MessagesTools.getCollectionMessages();
+        DBCollection collectionOut = bd.tools.RechercheTools.getCollectionIndexInverses();
+        
+        MapReduceCommand cmd = new MapReduceCommand(collectionMessages, mapFunction, reduceFunction, Noms.COLLECTION_INDEX_INVERSES, MapReduceCommand.OutputType.REPLACE, null);
+        cmd.setFinalize(finalizeFunction);
+        
+        // On définit N (le nombre de documents) comme variable globale
+        BasicDBObject n = new BasicDBObject();
+        n.put("N", collectionMessages.count());
+        cmd.setScope(n);
+        
+        // On applique le MapReduce
+        MapReduceOutput out = collectionMessages.mapReduce(cmd);
+        
+        System.out.println(out.toString());
+        System.out.println("OUTPUT FINI");
+        
+        for (DBObject o : out.results()) {
+        	System.out.println(o.toString());
+        }
+        System.out.println("LISTAGE FINI");
+        
+
+        JSONArray result = new JSONArray();
+        for (DBObject o : out.results()) {
+            try {
+                JSONObject message = new JSONObject(JSON.serialize(o));
+                result.put(message);
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+        return result;
+    }
+
 }
--- test/services/TestDivers.java
+++ test/services/TestDivers.java
@@ -9,7 +9,6 @@ public class TestDivers {
 	public static final int TEMPS_AVANT_DECONNEXION = 3600000; // 60 minutes
 	
 	public static void main(String[] args) throws ParseException, InterruptedException, UnknownHostException {
-		System.out.println(services.amis.ListerAmis.listerAmis("R59EB2DP22A87X57R18B3UK01WCSO522", "502", "0", "10"));
-		//clef=R59EB2DP22A87X57R18B3UK01WCSO522&id_utilisateur=502&index_debut=0&nombre_demandes=10
+		System.out.println(services.recherche.Recherche.MapReduce("Message").toString(4));
 	}
 }
